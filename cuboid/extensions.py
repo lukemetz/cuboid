34273bbb77a66954a12f692b1e0fb9eb26984a42
= <luke.metz@students.olin.edu>
2015-02-18 00:32:25 -0500

    Progress bar extension

--- cuboid/extensions.py
+++ cuboid/extensions.py
@@ -1,4 +1,6 @@
-from blocks.extensions import SimpleExtension
+from blocks.extensions import SimpleExtension, TrainingExtension
+
+from progressbar import ProgressBar, Percentage, ETA, Bar
 
 class LogToFile(SimpleExtension):
     def __init__(self, file_name, **kwargs):
@@ -14,3 +16,18 @@ class LogToFile(SimpleExtension):
 
     def do(self, which_callback, *args):
         self.main_loop.log.to_dataframe().to_csv(self.file_name)
+
+class EpochProgress(TrainingExtension):
+    def __init__(self, batch_per_epoch, **kwargs):
+        super(EpochProgress, self).__init__(**kwargs)
+        self.batch_per_epoch = batch_per_epoch
+
+    def before_epoch(self):
+        widgets = [Percentage(), ' ', Bar(),
+                   ' ', ETA(), ' ']
+        self.progress = ProgressBar(widgets=widgets, maxval=self.batch_per_epoch).start()
+        self.on_batch = 0
+
+    def before_batch(self, batch):
+        self.progress.update(self.on_batch)
+        self.on_batch += 1
