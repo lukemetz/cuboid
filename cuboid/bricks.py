4f39006e3002def062d96fdb3e26d637bc131756
= <luke.metz@students.olin.edu>
2015-02-21 00:17:37 -0500

    update dropout + filterpool

--- cuboid/bricks.py
+++ cuboid/bricks.py
@@ -1,12 +1,17 @@
 from blocks.bricks import Brick, Random
 from blocks.bricks.base import lazy, application
+from blocks.bricks import conv
+
 from blocks.utils import shared_floatx_zeros
 from blocks.utils import shared_floatx_zeros
 from blocks import config
 
 import theano
 from theano import tensor as T
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+
 import numpy as np
+
 
 class BatchNormalizationBase(Brick):
     """
@@ -122,6 +127,10 @@ class BatchNormalization(BatchNormalizationBase):
             return self.input_dim
         super(BatchNormalizationConv, self).get_dim(name)
 
+
+
+srng = RandomStreams(seed=32)
+
 class Dropout(Random):
     @lazy
     def __init__(self, p_drop, **kwargs):
@@ -132,7 +141,37 @@ class Dropout(Random):
     def apply(self, input_):
         if self.p_drop != 0:
             retain_prob = 1 - self.p_drop
-            mask = self.theano_rng.binomial(input_.shape, p=retain_prob, dtype='int32').astype('float32')
+            mask = srng.binomial(input_.shape, p=retain_prob, dtype='int32').astype('float32')
             return input_ / retain_prob * mask
         else:
             return input_
+
+class Flattener(conv.Flattener):
+    @lazy
+    def __init__(self, input_dim, **kwargs):
+        super(Flattener, self).__init__(**kwargs)
+        self.input_dim = input_dim
+
+    def get_dim(self, name):
+        if name == "input_":
+            return self.input_dim
+        if name == "output":
+            return np.prod(self.input_dim)
+
+class FilterPool(Brick):
+    @lazy
+    def __init__(self, fn, input_dim, **kwargs):
+        super(FilterPool, self).__init__(**kwargs)
+        self.fn = fn
+        self.input_dim = input_dim
+
+    @application(inputs=['input_'], outputs=['output'])
+    def apply(self, input_):
+        return self.fn(input_.reshape((input_.shape[0], input_.shape[1], -1)), axis=2)
+
+    def get_dim(self, name):
+        if name == "input_":
+            return self.input_dim
+        elif name == "output":
+            return self.input_dim[0]
+
--- tests/test_bricks.py
+++ tests/test_bricks.py
@@ -1,8 +1,9 @@
 from theano import tensor as T
 import theano
 import numpy as np
+from numpy.testing import assert_allclose
 
-from cuboid.bricks import Dropout
+from cuboid.bricks import Dropout, FilterPool
 
 def test_dropout():
     layer = Dropout(p_drop=0.1)
@@ -19,3 +20,21 @@ def test_dropout():
     # these values are different
     diff = np.sum((v2 - v1)**2)
     assert(diff >= .1)
+
+def test_filterpool():
+    layer = FilterPool(fn=T.mean, input_dim=(3, 4, 5))
+    layer.initialize()
+
+    x = T.tensor4()
+    y = layer.apply(x)
+
+    _func = theano.function([x], y)
+    x_val = np.zeros((1, 3, 4, 5)).astype(theano.config.floatX)
+    x_val[0, 0, 2, 3] = 19.0
+    x_val[0, 0, 1, 3] = 21.0
+    x_val[0, 1, 2, 3] = 10.0
+    x_val[0, 2, 2, 3] = 5.0
+
+    ret = _func(x_val)
+    assert_allclose(ret, np.array([[ 2, .5, 0.25]]))
+
