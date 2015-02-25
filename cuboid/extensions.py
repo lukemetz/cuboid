6b5dcb2a9eb5188d565853f8204431118d490f9e
= <luke.metz@students.olin.edu>
2015-02-24 23:33:21 -0500

    ExperimentSaver

--- cuboid/extensions.py
+++ cuboid/extensions.py
@@ -31,3 +31,46 @@ class EpochProgress(TrainingExtension):
     def before_batch(self, batch):
         self.progress.update(self.on_batch)
         self.on_batch += 1
+
+import shutil
+import os
+from blocks.dump import save_parameter_values
+import datetime
+
+class ExperimentSaver(TrainingExtension):
+    def __init__(self, dest_directory, src_directory, **kwargs):
+        super(ExperimentSaver, self).__init__(**kwargs)
+        self.dest_directory = dest_directory
+        self.src_directory = src_directory
+
+        self.params_path = os.path.join(self.dest_directory, 'params')
+
+    def params_path_for_epoch(self, i):
+        return os.path.join(self.params_path, str(i))
+
+    def before_training(self):
+        if os.path.exists(self.dest_directory):
+            time_string = datetime.datetime.now().strftime('%m-%d-%H-%M-%S') 
+            move_to = self.dest_directory + time_string + "_backup"
+            shutil.move(self.dest_directory, move_to)
+
+        os.mkdir(self.dest_directory)
+        src_path = os.path.join(self.dest_directory, 'src')
+
+        os.mkdir(self.params_path)
+
+        def ignore(path, names):
+            # TODO actually manage paths correctly
+            if path == self.dest_directory or path == './' + self.dest_directory:
+                return names
+            else:
+                return []
+
+        shutil.copytree(self.src_directory, src_path, ignore=ignore)
+
+    def after_epoch(self):
+        log = self.main_loop.log
+        epoch_done = log.status.epochs_done
+        params = self.main_loop.model.get_param_values()
+        path = self.params_path_for_epoch(epoch_done)
+        save_parameter_values(params, path)
---
+++ tests/test_extensions.py
@@ -0,0 +1,53 @@
+import numpy
+import theano
+import shutil
+
+from theano import tensor
+
+from blocks.datasets import ContainerDataset
+from blocks.main_loop import MainLoop
+from blocks.algorithms import GradientDescent, Scale
+from blocks.utils import shared_floatx
+from blocks.model import Model
+
+from blocks.extensions import FinishAfter
+
+from cuboid.extensions import ExperimentSaver
+
+floatX = theano.config.floatX
+
+def setup_mainloop(extension):
+    """Create a MainLoop, register the given extension, supply it with a
+        DataStream and a minimal model/cost to optimize.
+    """
+    features = [numpy.array(f, dtype=floatX)
+                for f in [[1, 2], [3, 4], [5, 6]]]
+    dataset = ContainerDataset(dict(features=features))
+
+    W = shared_floatx([0, 0], name='W')
+    x = tensor.vector('features')
+    cost = tensor.sum((x-W)**2)
+    cost.name = "cost"
+
+    algorithm = GradientDescent(cost=cost, params=[W],
+                                step_rule=Scale(1e-3))
+
+
+    main_loop = MainLoop(
+        model=Model(cost), data_stream=dataset.get_default_stream(),
+        algorithm=algorithm,
+        extensions=[
+            FinishAfter(after_n_epochs=1),
+            extension])
+
+    return main_loop
+
+def test_experiment_saver():
+    experiment_saver = ExperimentSaver(dest_directory='experimentSaverTest', src_directory='.')
+    main_loop = setup_mainloop(experiment_saver)
+
+    # be happy if this doesn't error
+    main_loop.run()
+
+    shutil.rmtree('experimentSaverTest')
+
