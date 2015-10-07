2e5814ee7be20d0a3a1120bf5abb7e44a093a7e7
luke metz <luke.s.metz@gmail.com>
2015-10-07 16:26:58 -0400

    foxhound style gru + Mutation3

--- cuboid/bricks/recurrent.py
+++ cuboid/bricks/recurrent.py
@@ -1,7 +1,11 @@
 from blocks.bricks.base import lazy, application
-from blocks.bricks import Linear, Initializable
+from blocks.bricks import Linear, Initializable, Tanh, Logistic
+from blocks.bricks.recurrent import BaseRecurrent, recurrent
 import numpy as np
 from blocks.initialization import NdarrayInitialization
+from blocks.utils import shared_floatx_nans, shared_floatx_zeros
+from blocks.roles import add_role, WEIGHT, INITIAL_STATE
+import theano.tensor as T
 
 
 class GruInitialization(NdarrayInitialization):
@@ -18,7 +22,7 @@ class GruInitialization(NdarrayInitialization):
             assert shape[1] % 2 == 0
             half_shape = (shape[0], shape[1]/2)
         else:
-            raise ValueError("Unexpected number of dims (%s)"%(str(shape)))
+            raise ValueError("Unexpected number of dims (%s)" % (str(shape)))
 
             raise ValueError("Gru not initialized. missing weights or biases")
 
@@ -98,3 +102,164 @@ class GatedRecurrentFork(Initializable):
         gate_inputs = self.input_to_gate_inputs.apply(input_)
 
         return inputs, gate_inputs
+
+
+class MUT3(BaseRecurrent, Initializable):
+    """ Mutation 3 as described by [1]
+
+    Reference
+    ---------
+    ..[1] http://jmlr.org/proceedings/papers/v37/jozefowicz15.pdf
+    """
+
+    @lazy(allocation=['dim'])
+    def __init__(self, dim, activation=None, gate_activation=None,
+                 **kwargs):
+        super(MUT3, self).__init__(**kwargs)
+        self.dim = dim
+
+        if not activation:
+            activation = Tanh()
+        if not gate_activation:
+            gate_activation = Logistic()
+        self.activation = activation
+        self.gate_activation = gate_activation
+
+        self.children = [activation, gate_activation]
+
+    @property
+    def state_to_state(self):
+        return self.parameters[0]
+
+    @property
+    def state_to_z(self):
+        return self.parameters[1]
+
+    @property
+    def state_to_r(self):
+        return self.parameters[2]
+
+    def get_dim(self, name):
+        if name == 'mask':
+            return 0
+        if name in ['inputs', 'states']:
+            return self.dim
+        if name in ['z', 'r']:
+            return self.dim
+        return super(MUT3, self).get_dim(name)
+
+    def _allocate(self):
+        self.parameters.append(shared_floatx_nans((self.dim, self.dim),
+                               name='state_to_state'))
+        self.parameters.append(shared_floatx_nans((self.dim, self.dim),
+                               name='state_to_z'))
+        self.parameters.append(shared_floatx_nans((self.dim, self.dim),
+                               name='state_to_r'))
+        self.parameters.append(shared_floatx_zeros((self.dim,),
+                               name="initial_state"))
+        for i in range(3):
+            if self.parameters[i]:
+                add_role(self.parameters[i], WEIGHT)
+        add_role(self.parameters[3], INITIAL_STATE)
+
+    def _initialize(self):
+        self.weights_init.initialize(self.state_to_state, self.rng)
+        self.weights_init.initialize(self.state_to_r, self.rng)
+        self.weights_init.initialize(self.state_to_z, self.rng)
+
+    @recurrent(sequences=['mask', 'inputs', 'z_inputs', 'r_inputs'],
+               states=['states'], outputs=['states'], contexts=[])
+    def apply(self, inputs, z_inputs, r_inputs, states, mask=None):
+        z = self.gate_activation.apply(
+            states.dot(self.activation.apply(self.state_to_z)) + z_inputs)
+        r = self.gate_activation.apply(
+            states.dot(self.state_to_r) + r_inputs)
+        states_reset = states * r
+        next_states = self.activation.apply(
+            states_reset.dot(self.state_to_state) + inputs)
+        next_states = (next_states * (1 - z) + states * z)
+        if mask:
+            next_states = (mask[:, None] * next_states +
+                           (1 - mask[:, None]) * states)
+        return next_states
+
+    @application(outputs=apply.states)
+    def initial_states(self, batch_size, *args, **kwargs):
+        return [T.repeat(self.parameters[3][None, :], batch_size, 0)]
+
+
+class GRU(BaseRecurrent, Initializable):
+    @lazy(allocation=['dim'])
+    def __init__(self, dim, activation=None, gate_activation=None,
+                 **kwargs):
+        super(GRU, self).__init__(**kwargs)
+        self.dim = dim
+
+        if not activation:
+            activation = Tanh()
+        if not gate_activation:
+            gate_activation = Logistic()
+        self.activation = activation
+        self.gate_activation = gate_activation
+
+        self.children = [activation, gate_activation]
+
+    @property
+    def state_to_state(self):
+        return self.parameters[0]
+
+    @property
+    def state_to_z(self):
+        return self.parameters[1]
+
+    @property
+    def state_to_r(self):
+        return self.parameters[2]
+
+    def get_dim(self, name):
+        if name == 'mask':
+            return 0
+        if name in ['inputs', 'states']:
+            return self.dim
+        if name in ['z', 'r']:
+            return self.dim
+        return super(GRU, self).get_dim(name)
+
+    def _allocate(self):
+        self.parameters.append(shared_floatx_nans((self.dim, self.dim),
+                               name='state_to_state'))
+        self.parameters.append(shared_floatx_nans((self.dim, self.dim),
+                               name='state_to_z'))
+        self.parameters.append(shared_floatx_nans((self.dim, self.dim),
+                               name='state_to_r'))
+        self.parameters.append(shared_floatx_zeros((self.dim,),
+                               name="initial_state"))
+        for i in range(3):
+            if self.parameters[i]:
+                add_role(self.parameters[i], WEIGHT)
+        add_role(self.parameters[3], INITIAL_STATE)
+
+    def _initialize(self):
+        self.weights_init.initialize(self.state_to_state, self.rng)
+        self.weights_init.initialize(self.state_to_r, self.rng)
+        self.weights_init.initialize(self.state_to_z, self.rng)
+
+    @recurrent(sequences=['mask', 'inputs', 'z_inputs', 'r_inputs'],
+               states=['states'], outputs=['states'], contexts=[])
+    def apply(self, inputs, z_inputs, r_inputs, states, mask=None):
+        z = self.gate_activation.apply(
+            states.dot(self.state_to_z) + z_inputs)
+        r = self.gate_activation.apply(
+            states.dot(self.state_to_r) + r_inputs)
+        states_reset = states * r
+        next_states = self.activation.apply(
+            states_reset.dot(self.state_to_state) + inputs)
+        next_states = (next_states * (1 - z) + states * z)
+        if mask:
+            next_states = (mask[:, None] * next_states +
+                           (1 - mask[:, None]) * states)
+        return next_states
+
+    @application(outputs=apply.states)
+    def initial_states(self, batch_size, *args, **kwargs):
+        return [T.repeat(self.parameters[3][None, :], batch_size, 0)]
