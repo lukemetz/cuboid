f61130e0d280e155208c41e66da06224d6481bca
luke metz <luke.s.metz@gmail.com>
2015-10-30 20:48:38 -0400

    Initial async forker.

--- cuboid/async.py
+++ cuboid/async.py
@@ -1,6 +1,6 @@
 from multiprocessing import Process
 from fuel.streams import ServerDataStream
-
+from subprocess import Popen
 
 def get_open_port():
     # http://stackoverflow.com/questions/2838244/get-open-tcp-port-in-python
@@ -31,3 +31,9 @@ def fork_to_background(make_datastream, sources):
                                   hwm=hwm,
                                   produces_examples=False)
     return datastream, proc
+
+def stream_from_file(filename, *args):
+    port = get_open_port()
+    proc = Popen(['python', filename, str(port)] + args, env=dict(os.environ, THEANO_FLAGS='device=cpu'))
+    stream = ServerDataStream(ss.sources, port=port, hwm=50, produces_examples=False)
+    return stream, proc
